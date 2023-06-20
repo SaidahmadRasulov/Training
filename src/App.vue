d6e804c0b5c7120504f7146b8fa2eb2a3e6b5941
SaidahmadRasulov <said,rasulov.2018@list.ru>
2023-06-20 13:34:38 +0500

    Initial

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <CNavbar />
+    <RouterView />
+  </div>
+</template>
+<script lang="ts">
+import CNavbar from './Components/CNavbar.vue'
+import { RouterView } from 'vue-router';
+
+export default {
+    components: {
+      CNavbar,
+      RouterView
+    }
+}
+</script>
+<style>
+  
+</style>
---
+++ src/Components/CNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="shadow-lg">
+        <div class="container mx-auto">
+            <nav class="navbar py-5 px-3 flex items-center justify-between">
+                <div class="navbar__logo">
+                    <img src="../assets/logo.png" alt="">
+                </div>
+                <div class="navbar__nav flex items-center gap-5">
+                    <button class=" text-white flex items-center gap-3 p-1 bg-indigo rounded-lg">
+                        <img src="../assets/bookmark.png" alt="">
+                        Favorites
+                    </button>
+                    <div class="account__image flex items-center">
+                        <img src="../assets/account.png" class="rounded-full" alt="">
+                        <i class="bx bx-chevron-down"></i>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+<script lang="ts">
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
